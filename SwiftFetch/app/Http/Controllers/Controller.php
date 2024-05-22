1e0f11b47515c947f0504aeb8f5373991f2f933f
Ger706 <frostggwp706@gmail.com>
2024-05-22 19:58:36 +0700

    Making Authentication

--- SwiftFetch/app/Http/Controllers/Controller.php
+++ SwiftFetch/app/Http/Controllers/Controller.php
@@ -5,9 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function showResponse($message =  null): JsonResponse
+    {
+        $response = [
+            'message' => $message,
+        ];
+
+        return response()->json($response);
+    }
 }
